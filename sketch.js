4a99b6fd135b46e0b403ffc42fabfa547f53f6da
Prayaspani <74231728+Prayaspani@users.noreply.github.com>
2020-11-30 15:50:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,30 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const  Bodies=Matter.Bodies
+
 function setup() {
-  createCanvas(1600,400);
-  speed=random(55,90);
-  weight=random(400,1500);
-  car=createSprite(50,200,50,50);
-  car.velocityX=speed;
-  car.shapeColor="red";
-  wall=createSprite(1400,200,60,200);
-  wall.shapeColor="brown";
+  createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world
+  var options={
+    restitution:1.0
+  }
+  ball=Bodies.circle(200,200,20,options);
+
+  World.add(world,ball)
+  var option2={
+    isStatic:true
+  }
+  ground=Bodies.rectangle(200,390,400,20,option2);
+  World.add(world,ground);
 }
 
 function draw() {
-  background(255,255,255); 
-  if(wall.x-car.x<car.width/2+wall.width/2) {
-car.velocityX=0;
-var deformation=0.5*weight*speed*speed/22509
-if(deformation>180){
-car.shapeColor=color(255,0,0);
-}
-if(deformation<180&&deformation>100){
-  car.shapeColor=color(230,230,0);
-  }
-  if(deformation<100){
-    car.shapeColor=color(0,255,0);
-    }
-  }
+  background("blue");  
+  Engine.update(engine)
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x,ball.position.y,20,20)
+  rectMode(CENTER)
+  rect(ground.position.x,ground.position.y,400,20)
   drawSprites();
 }
